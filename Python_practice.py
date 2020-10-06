58216dd916785ef5e4226ce2f9986ae11b50705c
Michela Biale <mbiale@Michela-Biales-MacBook-Pro.local>
2020-10-05 19:49:14 -0700

    Adding the election audit code.

---
+++ Python_practice.py
@@ -0,0 +1,13 @@
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # To do: read and analyze the data here.
+Next, we'll use the reader
