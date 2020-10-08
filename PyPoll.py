839e2912e911976609f49f5d238bf6bb7ea458b8
Michela Biale <mbiale@Michela-Biales-MacBook-Pro.local>
2020-10-08 15:50:26 -0700

    Adding the PyPoll_Challenge

--- PyPoll.py
+++ PyPoll.py
@@ -100,3 +100,4 @@ with open(file_to_save, "w") as txt_file:
             f"-------------------------\n")
 # Save the winning candidate's name to the text file.
     txt_file.write(winning_candidate_summary)
+
